8f8a9995eecca6f128cc3efd299f5ff8e156ee7f
GLCVLL <g.ioele96@outlook.it>
2023-07-19 17:00:55 +0200

    checked if form sended

--- index.php
+++ index.php
@@ -12,6 +12,18 @@ $generateRandomPassword = function ($length) {
 
     return $password;
 };
+ // Verifico se il form è stato inviato
+ if (isset($_GET['password_length'])) {
+    $password_length = intval($_GET['password_length']);
+    // Limito la lunghezza della password tra 5 e 50 caratteri
+    $password_length = max(5, min(50, $password_length));
+
+    // Genero la password casuale utilizzando la function
+    $generated_password = $generateRandomPassword($password_length);
+
+    // Mostrare la password all'utente attraverso un alert
+    echo "<div class='alert alert-success mt-3'>Password Generata: $generated_password</div>";
+}
 
 ?>
 
@@ -31,7 +43,7 @@ $generateRandomPassword = function ($length) {
     <h1 class="mt-5">Generatore di Password Casuale</h1>
         <form method="get" action="#" class="my-3">
             <div class="form-group">
-                <label for="password_length">Lunghezza Password:</label>
+                <label for="password_length">Scegli un numero per la lunghezza Password:</label>
                 <input type="number" id="password_length" name="password_length" class="form-control" min="5" max="50" required>
             </div>
             <button type="submit" class="btn btn-primary mt-3">Genera Password</button>
